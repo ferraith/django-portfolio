cf39adb8ebaa6384f75a84a8cdb23e03d6dcef44
ferraith <ferraith@gmail.com>
2017-12-15 19:25:15 +0100

    Added inline docu for PortfolioConfig class

--- portfolio/apps.py
+++ portfolio/apps.py
@@ -1,6 +1,13 @@
-"""Application configuration of the portfolio app."""
+"""Configuration of the portfolio application."""
 from django.apps import AppConfig
 
 
 class PortfolioConfig(AppConfig):
+    """Represents Django portfolio app.
+
+    Class representing Django portfolio application and its configuration.
+
+    :cvar name: name of app
+    """
+
     name = 'portfolio'
