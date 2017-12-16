51ccabcba350b02db258f1c7f59fee588be0c4b4
ferraith <ferraith@gmail.com>
2017-12-16 18:00:17 +0100

    Added share prices and stocks to admin site

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,9 @@
 """Admin interface of the portfolio app."""
 from django.contrib import admin
 
-from portfolio.models import Investment
+from portfolio.models import Investment, SharePrice, Stock
 
 
 admin.site.register(Investment)
+admin.site.register(SharePrice)
+admin.site.register(Stock)
