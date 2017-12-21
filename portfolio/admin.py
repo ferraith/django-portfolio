db6a31dbec91d8458b961b0dbc6a0050bb325012
ferraith <ferraith@gmail.com>
2017-12-21 19:39:41 +0100

    Added Asset, Bond and Fund model to admin site

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,9 +1,12 @@
 """Admin interface of the portfolio app."""
 from django.contrib import admin
 
-from portfolio.models import Investment, Portfolio, SharePrice, Stock, Transaction
+from portfolio.models import Asset, Bond, Fund, Investment, Portfolio, SharePrice, Stock, Transaction
 
 
+admin.site.register(Asset)
+admin.site.register(Bond)
+admin.site.register(Fund)
 admin.site.register(Investment)
 admin.site.register(Portfolio)
 admin.site.register(SharePrice)
